c1c0d81954d999c564b8d9f3f311bb489cc8feae
madoxxSad <eunice.sepulveda@udgvirtual.udg.mx>
2024-03-13 12:48:13 -0600

    Estilos menú definidos

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import image from '../images/logo.png'
 
 const Menu = () => {
   return (
     <div className="menu">
       <ul>
-        <li><a href="/">Inicio</a></li>
+        <li><a href="/"><img className='logo' src={image} alt="Logo" /></a></li>
         <li><a href="/cabanas">Cabañas</a></li>
         <li><a href="/actividades">Actividades</a></li>
         <li><a href="/preguntas">Preguntas</a></li>
