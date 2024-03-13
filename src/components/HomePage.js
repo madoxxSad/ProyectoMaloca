daba2e5a8ef19b25d88bbd0a2bf002e6e29cc996
madoxxSad <eunice.sepulveda@udgvirtual.udg.mx>
2024-03-13 13:08:08 -0600

    Estilos implementados en Inicio

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,12 +3,19 @@ import image from '../images/logo.png'
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Proyecto Maloca</h1>
-      <img src={image} alt="Logo" />
-      <p>Proyecto Maloca es un espacio que busca estar en armonía con 
+    <div className='homeContainer'>
+      <div className='homeTitle'>
+        <h1>Proyecto Maloca</h1>
+      </div>
+      <div className='homeLogo'>
+        <img src={image} className='hLogo' alt="Logo" />
+      </div>
+      <div className='homeText'>
+        <p>Proyecto Maloca es un espacio que busca estar en armonía con 
         el medio ambiente, procurando generar un menor impacto dentro 
         del área natural donde nos encontramos. </p>
+      </div>
+      
     </div>
   );
 };
