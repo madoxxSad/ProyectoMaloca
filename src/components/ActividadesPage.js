69aaf17430d5fc506e14986683a4647602bd40c1
madoxxSad <eunice.sepulveda@udgvirtual.udg.mx>
2024-03-14 19:13:45 -0600

    Texto de página Actividades

--- src/components/ActividadesPage.js
+++ src/components/ActividadesPage.js
@@ -2,9 +2,120 @@ import React from 'react';
 
 const ActividadesPage = () => {
   return (
-    <div>
-      <h1>Página de Actividades</h1>
-      <p>Contenido de la página de actividades.</p>
+    <div className='actContainer'>
+      <div className='actTitle'>
+        <h1>Actividades</h1>
+      </div>
+
+      <div className='actList'>
+        <div className='actividad'>
+          <div className='actImg'>
+
+          </div>
+          <div className='actDescription'>
+           <h2>
+              KAYAK
+           </h2>
+           <p>
+              En el lugar hay un estero en el cual que puede rentar un kayak 
+              y apreciar el paisaje del lugar y sus diferentes tipos de aves 
+              que paran a descansar en él.
+           </p>
+          </div>
+        </div>
+      </div>
+
+      <div className='actList'>
+        <div className='actividad'>
+          <div className='actImg'>
+      
+          </div>
+          <div className='actDescription'>
+            <h2>
+              SENDERISMO
+            </h2>
+            <p>
+              La propiedad cuenta con senderos en los cuales se podrán 
+              encontrar una gran variedad de árboles (parotas, ceibas, 
+              huges, etc), aves y algún que otro habitante del lugar 
+              (Tejones, iguanas, venados, pecaríes, etc.)
+              Si desean practicar senderismo, recomendamos traer calzado 
+              cómodo para caminar y algo para llevar agua en la caminata.
+            </p>
+          </div>
+        </div>
+      </div> 
+
+      <div className='actList'>
+        <div className='actividad'>
+          <div className='actImg'>
+      
+          </div>
+          <div className='actDescription'>
+            <h2>
+              FOTOGRAFÍA
+            </h2>
+            <p>
+             Si lo que te gusta es la fotografía de naturaleza, estás en 
+             el lugar ideal para que dejes volar tu imaginación. Ya sea con su flora, 
+             fauna o paisajes que te regala el lugar. 
+            </p>
+          </div>
+        </div>
+      </div> 
+
+      <div className='actList'>
+        <div className='actividad'>
+          <div className='actImg'>
+      
+          </div>
+          <div className='actDescription'>
+            <h2>
+              VISITAR PLAYAS CERCANAS
+            </h2>
+            <p>
+              Si tienes planeado quedarte varios días, podrás visitar diferentes 
+              playas que quedan cerca del lugar en auto.
+              <span>
+                El Bejuco (5 min): Famoso por sus botanas de mariscos y el color 
+                de los murales que hay pintados en sus casas.
+              </span>
+              <span>
+                Caleta (20 min ): Una playa familiar para ir a nadar en un mar tranquilo.
+              </span>
+              <span>
+                Nexpa (30 min): En este lugar podrás practicar surf o tomar alguna clase con un instructor.
+              </span>
+              <span>
+                Las pozas del atracón (45 min): Caminaras sobre una cañada para ir encontrando 
+                pozas en las cuales puedes nadar
+              </span>
+              <span>
+               Pichilinguillo (1 hr): Una playa donde encontrarás una playa cristalina y perfecta para nadar.  
+              </span>
+            </p>  
+          </div>
+        </div>
+      </div> 
+
+      <div className='actList'>
+        <div className='actividad'>
+          <div className='actImg'>
+      
+          </div>
+          <div className='actDescription'>
+            <h2>
+              DESCANSAR
+            </h2>
+            <p>
+              Si lo que buscas es descansar y dejar todo el estres atrás, Proyecto Maloca es el lugar 
+              ideal para descansar ya sea en tu cabaña o en alguna hamaca solo disfrutando del paisaje 
+              y sonidos del mar y alrededor.
+            </p>
+          </div>
+        </div>
+      </div> 
+
     </div>
   );
 };
