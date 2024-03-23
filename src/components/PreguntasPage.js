158d10a6391c8fcb299ffcb796d820cdc3f8cd11
madoxxSad <eunice.sepulveda@udgvirtual.udg.mx>
2024-03-23 16:20:31 -0600

    Preguntas Estilos terminados

--- src/components/PreguntasPage.js
+++ src/components/PreguntasPage.js
@@ -2,9 +2,181 @@ import React from 'react';
 
 const PreguntasPage = () => {
   return (
-    <div>
-      <h1>Página de Preguntas</h1>
-      <p>Contenido de la página de preguntas.</p>
+    <div className='prContainer'>
+      <div className='prTitle'>
+        <h1>Preguntas Frecuentes</h1>
+      </div>
+      <div className='preguntasContainer'>
+
+        <div className='prPregunta'>
+          <div className='pregunta'>
+            <p>
+              ¿Cómo llego a Proyecto Maloca?
+            </p>
+          </div>
+          <div className='respuesta'>
+            <p>
+              Lo ideal es llegar a la ciudad de Lázaro Cárdenas, Michoacán. 
+              Si vienes en coche privado, tomas camino ya sea por ¨La Mira¨ 
+              o por el boulevard playero que pasa por Playa Azul. 
+              Ambos llegan a la desviación de la ruta que sigue por la carretera 
+              de la Costa Michoacana. El lugar se encuentra a 10 minutos de Las Peñas, 
+              o bien, a 5 minutos de El bejuco. Recomendamos usar la aplicación por google maps.
+
+            </p>
+          </div>
+        </div>
+
+        <div className='prPregunta'>
+          <div className='pregunta'>
+            <p>
+              ¿Cuál es la ubicación de Proyecto Maloca?
+            </p>
+          </div>
+          <div className='respuesta'>
+            <p>
+              Estamos ubicados a 40 minutos de Lázaro Cardenas, sobre la costa Michoacana. 
+              Esta es la ubicación por google maps. La entrada de proyecto maloca queda en 
+              medio entre el poblado ¨popoyuta¨ y el puente de ¨la popoyuta¨ 
+              https://goo.gl/maps/KECgBzADWM4xfN3b8
+            </p>
+          </div>
+        </div>
+
+        <div className='prPregunta'>
+          <div className='pregunta'>
+            <p>
+              ¿Proyecto Maloca es mi lugar para pasar fin de semana/vacaciones?
+            </p>
+          </div>
+          <div className='respuesta'>
+            <p>
+            Recomendamos que si no eres una persona que disfruta caminar, 
+            la estancia puede ser un poco pesada para ti. El lugar implica un esfuerzo 
+            de movimiento para desplazarse a las cabañas (sobre la colina), estacionamiento, 
+            playa, cocina, ...
+            Hay que tomar consideración sobre niños menores o adultos mayores. 
+            No olvidemos que está en medio de una área natural, los caminos dentro 
+            del lugar son por brecha y los espacios en su mayoría son al aire libre. 
+            </p>
+          </div>
+        </div>
+
+        <div className='prPregunta'>
+          <div className='pregunta'>
+            <p>
+            ¿Puedo llevar niños?
+            </p>
+          </div>
+          <div className='respuesta'>
+            <p>
+              Recomendamos venir con niños mayores de 5 años (que ya caminen). 
+              Pueden venir de todas las edades, pero es importante recordar las 
+              condiciones del lugar: desniveles, caminos de brecha sobre la colina, 
+              espacios al aire libre en medio de la naturaleza y cuerpos de agua. 
+            </p>
+          </div>
+        </div>
+
+        <div className='prPregunta'>
+          <div className='pregunta'>
+            <p>
+              ¿Tienen servicio de restaurante?
+            </p>
+          </div>
+          <div className='respuesta'>
+            <p>
+              En el precio de la cabaña ya viene incluido un desayuno completo 
+              que se sirve de 9 am a 10:30 am y comida de mar que se sirve a las 3 pm a 4 pm.
+              Vemos que la mayoria de los huéspedes quedan satisfechos con 2 comidas al dia, 
+              ya que son porciones abundantes. En caso de querer cena tienen un costo extra.
+            </p>
+          </div>
+        </div>
+
+        <div className='prPregunta'>
+          <div className='pregunta'>
+            <p>
+              Hay alguna enrramada/restaurante o tienda cerca del lugar?
+            </p>
+          </div>
+          <div className='respuesta'>
+            <p>
+              Saliendo en coche de la puerta de acceso de Proyecto Maloca sobre la carretera, 
+              a mano derecha llegarás en 5 minutos al poblado ¨El Bejuco¨ conocido por su botana 
+              y ostiones en temporada. Aquí puedes encontrar enrramadas al nivel del mar y tiendas 
+              para antojos como galletas, frituras, refrescos, hielo y cerveza. Sobre la misma dirección, 
+              a 5 minutos de el Bejuco llegarás a ¨Las Peñas¨, igual puedes encontrar enramadas al 
+              nivel del mar y la tienda de ¨Don Luis¨ si quieres comprar: verdura, fruta, 
+              huevos, jamón, chorizo y demás antojos. Recuerda que para salir de Proyecto Maloca 
+              se debe caminar por brecha 7 minutos al igual que para entrar una vez en el estacionamiento. 
+            </p>
+          </div>
+        </div>
+
+        <div className='prPregunta'>
+          <div className='pregunta'>
+            <p>
+              ¿Puedo llevar a mi mascota?
+            </p>
+          </div>
+          <div className='respuesta'>
+            <p>
+              Claro puedes traerla. Es necesario estar al pendiente de ella. 
+              Hay que traerla/o con correa en áreas comunes donde estén los 
+              demás huéspedes y procurar limpiar sus heces para evitar malos 
+              olores sobre los caminos. Avísanos si te acompañan antes de reservar. 
+              El costo por día es de $100 por mascota
+            </p>
+          </div>
+        </div>
+
+        <div className='prPregunta'>
+          <div className='pregunta'>
+            <p>
+              ¿Cuentan con wifi o internet?
+            </p>
+          </div>
+          <div className='respuesta'>
+            <p>
+              Tenemos wifi en un area designada, pero la velocidad es un poco lenta por la ubicación.
+              Para hacer llamadas o enviar mensaje (red 3G), te podemos indicar una ruta para llegar 
+              a un punto de señal sobre la colina a 15 minutos del lugar. 
+            </p>
+          </div>
+        </div>
+
+        <div className='prPregunta'>
+          <div className='pregunta'>
+            <p>
+              ¿Se puede nadar en el mar?
+            </p>
+          </div>
+          <div className='respuesta'>
+            <p>
+              Estamos en la parte del pacífico, en una playa larga de mar abierto. 
+              Si sabes nadar no hay de qué preocuparse a menos que el oleaje sea muy alto 
+              (Dependiendo temporadas). Lo ideal para nadar cuando está más tranquilo el 
+              mar es de Octubre a Marzo. La ventaja es que puedes nadar en el estero o dar 
+              un agradable paseo en kayak al atardecer. 
+            </p>
+          </div>
+        </div>
+
+        <div className='prPregunta'>
+          <div className='pregunta'>
+            <p>
+              ¿Cuál es la mejor temporada para visitar Proyecto Maloca?
+            </p>
+          </div>
+          <div className='respuesta'>
+            <p>
+              De Octubre a Marzo, el clima es mas agradable por la noche.
+              También se puede nadar en el mar por ser la marea baja del año.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
